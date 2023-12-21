d50cb316d1fd4f81c7ef4924fa080e493040d85f
Nguyen Trung Kien <trungkien11042004@gmail.com>
2023-12-22 00:25:17 +0700

    some insert sql

---
+++ query/insert_country.sql
@@ -0,0 +1,20 @@
+--Insert some countries
+INSERT INTO countries VALUES('USA','United States');
+INSERT INTO countries VALUES('JPN','Japan');
+INSERT INTO countries VALUES('GBR','United Kingdom');
+INSERT INTO countries VALUES('CHN','China');
+INSERT INTO countries VALUES('FRA','France');
+INSERT INTO countries VALUES('DEU','Germany');
+INSERT INTO countries VALUES('AUS','Australia');
+INSERT INTO countries VALUES('CAN','Canada');
+INSERT INTO countries VALUES('THA','Thailand');
+INSERT INTO countries VALUES('TUR','Turkey');
+INSERT INTO countries VALUES('ZAF','South Africa');
+INSERT INTO countries VALUES('SGP','Singapore');
+INSERT INTO countries VALUES('IRL','Ireland');
+INSERT INTO countries VALUES('MEX','Mexico');
+INSERT INTO countries VALUES('IND','India');
+INSERT INTO countries VALUES('CHE','Switzerland');
+INSERT INTO countries VALUES('KOR','South Korea');
+INSERT INTO countries VALUES('HKG','Hong Kong');
+INSERT INTO countries VALUES('VNM','Viet Nam');
