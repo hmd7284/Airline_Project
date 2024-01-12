675e9fd4114315147fa4498e1350bb2653664ded
Ha Minh Duc <117496050+hmd7284@users.noreply.github.com>
2024-01-12 10:38:08 +0700

    Add search flight to home page

--- routes/home.js
+++ routes/home.js
@@ -7,10 +7,6 @@ router.get("/", (req, res) => {
   res.redirect("/home");
 });
 
-router.get("/home", (req, res) => {
-  res.render("home.ejs");
-});
-
 router.get("/about", (req, res) => {
   res.render("about.ejs");
 });
@@ -54,10 +50,9 @@ async function SearchResults(
   const client = await db.connect();
   try {
     const result = await client.query(
-      "SELECT f.flight_code, f.departure_date, f.departure_time, r.origin, r.destination, a.aircraft_code, f.status, a.aircraft_name, af1.price as economy_price, af2.price as business_price FROM aircraft a JOIN flight_schedule f ON a.aircraft_code = f.aircraft JOIN route r ON f.route = r.route_code JOIN airfare af1 ON af1.route = r.route_code AND af1.TYPE = 'Economy' JOIN airfare af2 ON af2.route = r.route_code AND af2.TYPE = 'Business' WHERE r.origin = $1 AND r.destination = $2 AND f.departure_date = $3 AND f.status = 'Success' AND ((CAST(f.departure_date || ' ' || f.departure_time AS timestamp))::timestamptz >= current_timestamp + INTERVAL '4 hours') AND (f.business_seat + f.economy_seat >= $4) ORDER BY f.departure_time ASC",
+      "SELECT f.flight_code, f.departure_date, f.departure_time, r.origin, r.destination, a.aircraft_code, f.status, a.aircraft_name, af1.price as economy_price, af2.price as business_price FROM aircraft a JOIN flight_schedule f ON a.aircraft_code = f.aircraft JOIN route r ON f.route = r.route_code JOIN airfare af1 ON af1.route = r.route_code AND af1.TYPE = 'Economy' JOIN airfare af2 ON af2.route = r.route_code AND af2.TYPE = 'Business' WHERE r.origin = $1 AND r.destination = $2 AND f.departure_date = $3 AND f.status = 'Success' AND (f.business_seat + f.economy_seat >= $4) ORDER BY f.departure_time ASC",
       [origin, destination, departureDate, numberOfTickets],
     );
-    console.log(result.rows);
     return result.rows;
   } catch (error) {
     console.error("Error during booking:", error);
@@ -69,8 +64,46 @@ async function SearchResults(
 
 router.get("/home", async (req, res) => {
   try {
+    const { origin, destination, departureDate, numberOfTickets } = req.query;
+    const userInput = req.session.userInput || {};
     const airports = await fetchAirportsFromDatabase();
-    res.render("home.ejs", { airports });
+    if (origin && destination && departureDate && numberOfTickets) {
+      if (origin === destination) {
+        req.session.userInput = req.query;
+        req.flash("error", "Origin and destination cannot be the same!");
+        // res.render("user_booking.ejs", {
+        //   airports,
+        //   transactions,
+        //   results: [],
+        //   scrollToResults: false,
+        //   scrollToSearch: true,
+        // });
+        res.redirect("/home");
+        return;
+      }
+
+      const results = await SearchResults(
+        origin,
+        destination,
+        departureDate,
+        numberOfTickets,
+      );
+
+      res.render("home.ejs", {
+        airports,
+        userInput,
+        results,
+        scrollToResults: true,
+      });
+    }
+
+    res.render("home.ejs", {
+      airports,
+      userInput,
+      results: [],
+      scrollToResults: false,
+      message: req.flash("error"),
+    });
   } catch (error) {
     console.error("Error loading home page:", error);
     res.status(500).json({ message: "Internal server error" });
